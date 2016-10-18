751ccad692210a8a51090da67bce0373ba8486d8
johansalazar7 <johansalazarburgos27@gmail.com>
2016-10-18 18:04:56 -0500

    ultimo cambio agenda

--- public/js2/agenda.js
+++ public/js2/agenda.js
@@ -1,81 +1,24 @@
 //<![CDATA[
-<<<<<<< HEAD
-
-    $(function() {
-        $('.calendar').pignoseCalendar({
-            select: function(date, obj) {
-                obj.calendar.parent().next().show().text('Dias Seleccionados ' +
-                (date[0] === null? 'null':date[0].format('YYYY-MM-DD')) +
-                '.');
-            }
-        });
-
-        $('.input-calendar').pignoseCalendar({
-            buttons: true,
-        });
-
-        var $btn = $('.btn-calendar').pignoseCalendar({
-            modal: true,
-            buttons: true,
-            apply: function(date) {
-                $btn.next().show().text('You applied date ' + date + '.');
-            }
-        });
-
-        $('.calendar-dark').pignoseCalendar({
-            theme: 'dark',
-            select: function(date, obj) {
-                obj.calendar.parent().next().show().text('Dias seleccionados ' +
-                (date[0] === null? 'null':date[0].format('YYYY-MM-DD')) +
-                '.');
-            }
-        });
-
-        $('.multi-select-calendar').pignoseCalendar({
-            multiple: true,
-            select: function(date, obj) {
-                obj.calendar.parent().next().show().text('Dias seleccionados ' +
-                    (date[0] === null? 'null':date[0].format('YYYY-MM-DD')) +
-                    '~' +
-                    (date[1] === null? 'null':date[1].format('YYYY-MM-DD')) +
-                    '.');
-            }
-        });
-
-        $('.toggle-calendar').pignoseCalendar({
-            toggle: true,
-            select: function(date, obj) {
-                var $target = obj.calendar.parent().next().show().html('Dias seleccionados ' +
-                (date[0] === null? 'null':date[0].format('YYYY-MM-DD')) +
-                '.' +
-                '<br /><br />' +
-                '<strong>Datos</strong><br /><br />' +
-                '<div class="active-dates"></div>');
-
-                for(var idx in obj.storage.activeDates) {
-                    var date = obj.storage.activeDates[idx];
-                    if(typeof date !== 'string') {
-                        continue;
-                    }
-
-
-                    $target.find('.active-dates').append('<span name="agenda" class="label label-default">' + date + '</span>');
-                }
-            }
-
-        });
-
-        $('.language-calendar').each(function() {
-            var $this = $(this);
-            console.log($this);
-            var lang = $this.data('lang');
-            $this.pignoseCalendar({
-                lang: lang
-            });
-        });
-=======
 $(function() {
+  $('.calendar').pignoseCalendar({
+    select: function(date, obj) {
+      obj.calendar.parent().next().show().text('Dias Seleccionados ' +
+        (date[0] === null ? 'null' : date[0].format('YYYY-MM-DD')) +
+        '.');
+    }
+  });
 
+  $('.input-calendar').pignoseCalendar({
+    buttons: true,
+  });
+
+  var $btn = $('.btn-calendar').pignoseCalendar({
+    modal: true,
+    buttons: true,
+    apply: function(date) {
+      $btn.next().show().text('You applied date ' + date + '.');
+    }
+  });
 
   $('.calendar-dark').pignoseCalendar({
     theme: 'dark',
@@ -133,7 +76,6 @@ $(function() {
     var lang = $this.data('lang');
     $this.pignoseCalendar({
       lang: lang
->>>>>>> 5be9e5b7a00c4b006f3fd4f921ed0656d346b581
     });
   });
 });
@@ -149,6 +91,7 @@ function guardar() {
       var algo = $(this).text();
 
       arrayMe[index] = $(this).text();
+      // algo
 
       // $(this).removeClass();
       // $(this).addClass("parrafo");
@@ -177,8 +120,6 @@ function guardar() {
       console.log("complete");
     });
 
-<<<<<<< HEAD
-=======
 }
 
 //sacamos datos con el onchange
@@ -197,7 +138,6 @@ document.getElementById('medicos').addEventListener('change', function(){
   //  var munic = $('#municipio').empty();
 
 
->>>>>>> 5be9e5b7a00c4b006f3fd4f921ed0656d346b581
 
 
 
